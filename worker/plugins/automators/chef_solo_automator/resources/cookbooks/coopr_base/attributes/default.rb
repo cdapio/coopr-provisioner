b4d3665dffab586c1b43fbc2b82faf07e600be58
Chris Gianelloni <chris@cask.co>
2015-01-09 12:52:16 -0800

    Ensure sysadmin users are allowed without password, by default

--- worker/plugins/automators/chef_solo_automator/resources/cookbooks/coopr_base/attributes/default.rb
+++ worker/plugins/automators/chef_solo_automator/resources/cookbooks/coopr_base/attributes/default.rb
@@ -1,2 +1,6 @@
 default['base']['use_epel'] = true
 default['apt']['compile_time_update'] = true
+# Default group used in Chef's users::sysadmins recipe
+default['authorization']['sudo']['groups'] = 'sysadmin'
+default['authorization']['sudo']['passwordless'] = true
+default['authorization']['sudo']['include_sudoers_d'] = true
