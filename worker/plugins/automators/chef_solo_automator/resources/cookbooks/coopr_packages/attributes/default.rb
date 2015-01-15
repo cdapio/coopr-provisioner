0b56f7a43c3b7b317ac9c6283cb9baf679b34090
Chris Gianelloni <chris@cask.co>
2015-01-14 19:20:57 -0800

    Cookbook to allow install/upgrade/remove of packages

---
+++ worker/plugins/automators/chef_solo_automator/resources/cookbooks/coopr_packages/attributes/default.rb
@@ -0,0 +1,6 @@
+default['coopr_packages']['debian']['install'] = []
+default['coopr_packages']['debian']['upgrade'] = []
+default['coopr_packages']['debian']['remove'] = []
+default['coopr_packages']['rhel']['install'] = []
+default['coopr_packages']['rhel']['upgrade'] = []
+default['coopr_packages']['rhel']['remove'] = ['yum-cron']
