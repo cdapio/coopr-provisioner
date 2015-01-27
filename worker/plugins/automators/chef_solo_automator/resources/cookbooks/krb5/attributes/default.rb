1a67bcd13acd25e53f6ab7969f035eb6fdbef803
Chris Gianelloni <chris@cask.co>
2015-01-27 11:01:02 -0800

    Import krb5 version 1.1.0

--- worker/plugins/automators/chef_solo_automator/resources/cookbooks/krb5/attributes/default.rb
+++ worker/plugins/automators/chef_solo_automator/resources/cookbooks/krb5/attributes/default.rb
@@ -19,6 +19,7 @@
 # limitations under the License.
 #
 
+# Platform-specific configuration
 case node['platform_family']
 when 'rhel'
   default['krb5']['packages'] = %w(krb5-libs krb5-workstation pam pam_krb5 authconfig)
@@ -29,6 +30,7 @@ when 'rhel'
   default['krb5']['kadmin']['packages'] = %w(krb5-server)
   default['krb5']['kdc']['service_name'] = 'krb5kdc'
   default['krb5']['kdc']['packages'] = %w(krb5-server krb5-server-ldap)
+  default['krb5']['devel']['packages'] = %w(krb5-devel)
 when 'debian'
   default['krb5']['packages'] = %w(libpam-krb5 libpam-runtime libkrb5-3 krb5-user)
   default['krb5']['authconfig'] = 'pam-auth-update --package krb5'
@@ -38,6 +40,7 @@ when 'debian'
   default['krb5']['kadmin']['packages'] = %w(krb5-admin-server)
   default['krb5']['kdc']['service_name'] = 'krb5-kdc'
   default['krb5']['kdc']['packages'] = %w(krb5-kdc krb5-kdc-ldap)
+  default['krb5']['devel']['packages'] = %w(libkrb5-dev)
 when 'suse'
   default['krb5']['packages'] = %w(krb5 pam_krb5 pam-config)
   default['krb5']['authconfig'] = 'pam-config --add --krb5'
@@ -46,8 +49,11 @@ else
   default['krb5']['authconfig'] = ''
   default['krb5']['kadmin']['packages'] = []
   default['krb5']['kdc']['packages'] = []
+  default['krb5']['devel']['packages'] = []
 end
 
+# TODO: deprecate these for future removal
+# Legacy attributes
 default['krb5']['default_logging'] = 'FILE:/var/log/krb5libs.log'
 default['krb5']['default_realm'] = node['domain'].upcase
 default['krb5']['realms'] = [node['domain']]
@@ -59,13 +65,16 @@ default['krb5']['forwardable'] = 'true'
 
 default_realm =
   if node['krb5'].key?('krb5_conf') && node['krb5']['krb5_conf'].key?('libdefaults') &&
-    node['krb5']['krb5_conf']['libdefaults'].key?('default_realm')
+     node['krb5']['krb5_conf']['libdefaults'].key?('default_realm')
     node['krb5']['krb5_conf']['libdefaults']['default_realm'].upcase
   elsif node['krb5']['default_realm']
     node['krb5']['default_realm'].upcase
   else
     'LOCAL'
   end
+
+# Default location for keytabs generated from LWRP
+default['krb5']['keytabs_dir'] = '/etc/security/keytabs'
 
 # Client Packages
 default['krb5']['client']['packages'] = node['krb5']['packages']
