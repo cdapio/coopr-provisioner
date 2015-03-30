dd98e62a961326c7ea60d4f4909e3b9c36fcf1fa
Chris Gianelloni <chris@cask.co>
2015-03-30 16:54:31 -0700

    Import nodejs version 2.3.0

--- lib/provisioner/worker/plugins/automators/chef_solo_automator/resources/cookbooks/nodejs/metadata.rb
+++ lib/provisioner/worker/plugins/automators/chef_solo_automator/resources/cookbooks/nodejs/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'guilhem@lettron.fr'
 license 'Apache 2.0'
 description 'Installs/Configures nodejs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.2.0'
+version '2.3.0'
 
 conflicts 'node'
 
@@ -12,8 +12,9 @@ depends 'yum-epel'
 depends 'build-essential'
 depends 'ark'
 depends 'apt'
+depends 'homebrew'
 
-%w(debian ubuntu centos redhat smartos).each do |os|
+%w(debian ubuntu centos redhat smartos mac_os_x).each do |os|
   supports os
 end
 
