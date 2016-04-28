77db0bc6a95be188d04f0672b2a2036f1376e7a2
Chris Gianelloni <chris@cask.co>
2016-04-28 16:40:54 -0400

    Follow Chef standards for attribute tree naming

--- lib/provisioner/worker/plugins/automators/chef_solo_automator/resources/cookbooks/coopr_base/recipes/default.rb
+++ lib/provisioner/worker/plugins/automators/chef_solo_automator/resources/cookbooks/coopr_base/recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: coopr_base
 # Recipe:: default
 #
-# Copyright © 2013-2014 Cask Data, Inc.
+# Copyright © 2013-2016 Cask Data, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,12 +22,12 @@ case node['platform_family']
 when 'debian'
   include_recipe 'apt::default'
 when 'rhel'
-  include_recipe 'yum-epel::default' if node['base']['use_epel'].to_s == 'true'
+  include_recipe 'yum-epel::default' if node['coopr_base']['use_epel'].to_s == 'true'
 end
 
 # We always run our dns, firewall, hosts, and packages cookbooks
 %w(dns firewall hosts packages).each do |cb|
-  include_recipe "coopr_#{cb}::default" unless node['base'].key?("no_#{cb}") && node['base']["no_#{cb}"].to_s == 'true'
+  include_recipe "coopr_#{cb}::default" unless node['coopr_base'].key?("no_#{cb}") && node['coopr_base']["no_#{cb}"].to_s == 'true'
 end
 
 # Ensure user ulimits are enabled
@@ -36,14 +36,14 @@ include_recipe 'ulimit::default'
 # Add users in the sysadmins group and give them sudo access
 # WARNING - Any user management done here must include any users defined in the Coopr Providers, or not interfere with
 #   their sudo permissions
-unless node['base'].key?('no_users') && node['base']['no_users'].to_s == 'true'
+unless node['coopr_base'].key?('no_users') && node['coopr_base']['no_users'].to_s == 'true'
   %w(chef-solo-search users::sysadmins).each do |cb|
     include_recipe cb
   end
   defined_users = search('users', 'groups:sysadmin AND NOT action:remove')
   if defined_users.empty?
     Chef::Log.warn('No users defined in group sysadmins, skipping sudo')
-  elsif node['base'].key?('no_sudo') && node['base']['no_sudo'].to_s == 'true'
+  elsif node['coopr_base'].key?('no_sudo') && node['coopr_base']['no_sudo'].to_s == 'true'
     Chef::Log.info('Skipping sudo due to no_sudo attribute')
   else
     include_recipe 'sudo'
