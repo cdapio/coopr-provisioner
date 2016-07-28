6a5cacd8154bf28d03f7cb735bf019553015be03
Chris Gianelloni <chris@cask.co>
2016-07-28 10:03:49 -0400

    Ensure override['build-essential']['compile_time'] = true (#134)

--- lib/provisioner/worker/plugins/automators/chef_solo_automator/resources/cookbooks/coopr_dns/attributes/dnsimple.rb
+++ lib/provisioner/worker/plugins/automators/chef_solo_automator/resources/cookbooks/coopr_dns/attributes/dnsimple.rb
@@ -3,3 +3,5 @@ default['dnsimple']['fog_version'] = '1.21.0'
 # Our configuration
 default['coopr_dns']['dnsimple']['databag_name'] = 'creds'
 default['coopr_dns']['dnsimple']['databag_item'] = 'dnsimple'
+# Ensure we run build-essential at compile-time
+override['build-essential']['compile_time'] = true
