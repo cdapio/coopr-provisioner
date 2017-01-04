4c3a0fc2ce5a4cc34100f48c50a7a25ead03bc91
Derek Wood <derek@cask.co>
2017-01-04 00:40:59 -0800

    add attributes for package manager options on coopr_packages installs and upgrades

--- lib/provisioner/worker/plugins/automators/chef_solo_automator/resources/cookbooks/coopr_packages/recipes/default.rb
+++ lib/provisioner/worker/plugins/automators/chef_solo_automator/resources/cookbooks/coopr_packages/recipes/default.rb
@@ -17,22 +17,29 @@
 # limitations under the License.
 #
 
+pf = node['platform_family']
+
 # Install common packages
 %w(remove install upgrade).each do |act|
   node['coopr_packages']['common'][act].each do |cb|
     package cb do
       action act.to_sym
+      if node['coopr_packages'].key?(pf) && node['coopr_packages'][pf].key?('options')
+        options node['coopr_packages'][pf]['options']
+      end
     end
   end
 end
 
 # Install platform-specific packages
-pf = node['platform_family']
 %w(remove install upgrade).each do |act|
   if node['coopr_packages'].key?(pf) && node['coopr_packages'][pf].key?(act)
     node['coopr_packages'][pf][act].each do |cb|
       package cb do
         action act.to_sym
+        if node['coopr_packages'][pf].key?('options')
+          options node['coopr_packages'][pf]['options']
+        end
       end
     end
   end
@@ -42,13 +49,13 @@ end
 case pf
 when 'debian'
   execute 'update-apt-packages' do
-    command 'apt-get update && apt-get upgrade -y && apt-get install -y unattended-upgrades'
+    command "apt-get #{node['coopr_packages']['debian']['options']} update && apt-get #{node['coopr_packages']['debian']['options']} upgrade && apt-get #{node['coopr_packages']['debian']['options']} install unattended-upgrades"
     environment 'DEBIAN_FRONTEND' => 'noninteractive'
     not_if { node['coopr_packages']['skip_updates'].to_s == 'true' }
   end
 when 'rhel'
   execute 'update-yum-packages' do
-    command 'yum makecache && yum upgrade -y'
+    command "yum #{node['coopr_packages']['rhel']['options']} makecache && yum #{node['coopr_packages']['rhel']['options']} upgrade"
     not_if { node['coopr_packages']['skip_updates'].to_s == 'true' }
   end
 end
