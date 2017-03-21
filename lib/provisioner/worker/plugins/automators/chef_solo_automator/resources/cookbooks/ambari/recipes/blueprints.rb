d6b58c84c79ae77740981fc3fa08e030e5849923
Chris Gianelloni <chris@cask.co>
2017-03-21 16:47:58 -0400

    Import ambari version 0.4.0

--- lib/provisioner/worker/plugins/automators/chef_solo_automator/resources/cookbooks/ambari/recipes/blueprints.rb
+++ lib/provisioner/worker/plugins/automators/chef_solo_automator/resources/cookbooks/ambari/recipes/blueprints.rb
@@ -17,17 +17,50 @@
 # limitations under the License.
 #
 
-if Chef::Config[:solo]
-  Chef::Log.warn('This recipe uses search. Chef Solo does not support search.')
-else
-  ambari_server_fqdn = node['ambari']['server_fqdn'] || search('node', 'run_list:recipe\[ambari\:\:server\] AND chef_environment:' + node.chef_environment).first['fqdn']
-end
+package 'curl'
+
+# Get Ambari Server FQDN
+#
+# Logic:
+# - is server set?
+# - am I server?
+# - must search
+#   - can I search?
+#   - search
+ambari_server_fqdn =
+  if node['ambari'].key?('server_fqdn') # Server is set
+    node['ambari']['server_fqdn']
+  elsif node['recipes'].include?('ambari::server') # Server is me
+    node['fqdn']
+  else # must search
+    do_search = if Chef::Config[:solo] # chef-solo can't search, by default
+                  if node['recipes'].include?('chef-solo-search::default')
+                    true # it can with chef-solo-search
+                  else
+                    false
+                  end
+                else
+                  true
+                end
+    if do_search == true
+      search('node', 'recipes:ambari\:\:server AND chef_environment:' + node.chef_environment).first['fqdn'] # ~FC010
+    end
+  end
+
 basic_auth_parameters = "--user #{node['ambari']['admin_user']}:#{node['ambari']['admin_password']}"
 
+file '/tmp/blueprint.json' do
+  content Chef::JSONCompat.to_json_pretty(node['ambari']['blueprints']['blueprint_json'].to_hash)
+end
+
+file '/tmp/cluster.json' do
+  content Chef::JSONCompat.to_json_pretty(node['ambari']['blueprints']['cluster_json'].to_hash)
+end
+
 execute 'Init Blueprints' do
-  command "curl #{basic_auth_parameters} -H 'X-Requested-By:ambari-cookbook' --data '#{node['ambari']['blueprints']['blueprint_json'].to_json}' #{ambari_server_fqdn}:8080/api/v1/blueprints/#{node['ambari']['blueprints']['blueprint_name']}"
+  command "curl #{basic_auth_parameters} -H 'X-Requested-By:ambari-cookbook' --data @/tmp/blueprint.json #{ambari_server_fqdn}:8080/api/v1/blueprints/#{node['ambari']['blueprints']['blueprint_name']}"
 end
 
 execute 'Init Cluster' do
-  command "curl #{basic_auth_parameters} -H 'X-Requested-By:ambari-cookbook' --data '#{node['ambari']['blueprints']['cluster_json'].to_json}' #{ambari_server_fqdn}:8080/api/v1/clusters/#{node['ambari']['blueprints']['cluster_name']}"
+  command "curl #{basic_auth_parameters} -H 'X-Requested-By:ambari-cookbook' --data @/tmp/cluster.json #{ambari_server_fqdn}:8080/api/v1/clusters/#{node['ambari']['blueprints']['cluster_name']}"
 end
--- lib/provisioner/worker/plugins/automators/chef_solo_automator/resources/cookbooks/ambari/recipes/server.rb
+++ lib/provisioner/worker/plugins/automators/chef_solo_automator/resources/cookbooks/ambari/recipes/server.rb
@@ -17,21 +17,70 @@
 # limitations under the License.
 #
 
+include_recipe 'ambari::default'
 include_recipe 'ambari::setup_package_manager'
 
-%w(ambari-server postgresql).each do |pack|
-  package pack
+package 'ambari-server'
+
+# This could probably be a bit more clean instead of having a nested case.
+# it works now, but will be ugly if we add support for all cases.
+case node['ambari']['database']['type']
+when 'embedded'
+  db_opts = ''
+when 'mysql'
+  db_opts = "--database=#{node['ambari']['database']['type']} \
+    --databaseport=#{node['ambari']['database']['port']} \
+    --databasehost=#{node['ambari']['database']['host']} \
+    --databasename=#{node['ambari']['database']['name']} \
+    --databaseusername=#{node['ambari']['database']['username']} \
+    --databasepassword=#{node['ambari']['database']['password']}"
+when 'mssql', 'oracle', 'postgres', 'sqlanywhere'
+  # needs db_opts cause the options differ from each db type.
+  raise "#{node['ambari']['database']['type']} is not supported yet"
+end
+
+# this case sets the jdbc driver package name for each distro and DB type (unless database type is 'embedded').
+unless node['ambari']['database']['type'] == 'embedded'
+  case node['platform']
+  when 'ubuntu', 'debian'
+    jdbcpkg = 'libmysql-java' if node['ambari']['database']['type'] == 'mysql'
+  when 'redhat', 'centos', 'amazon', 'scientific'
+    jdbcpkg = 'mysql-connector-java' if node['ambari']['database']['type'] == 'mysql'
+  when 'suse'
+    jdbcpkg = 'mysql-connector-java' if node['ambari']['database']['type'] == 'mysql'
+  end
+end
+
+# install jdbc driver.
+unless node['ambari']['database']['type'] == 'embedded'
+  if node['ambari']['jdbc']['url'] == ''
+    package jdbcpkg
+  else
+    remote_file node['ambari']['jdbc']['path'] do
+      source node['ambari']['jdbc']['url']
+      not_if { ::File.exist?(node['ambari']['jdbc']['path']) }
+    end
+  end
 end
 
 execute 'setup ambari-server' do
-  command 'ambari-server setup -s'
+  command "ambari-server setup #{db_opts} -s && touch /etc/ambari-server/.configured"
+  creates '/etc/ambari-server/.configured'
 end
 
-service 'postgresql' do
-  action [:enable, :start]
+if node['ambari']['database']['type'] == 'embedded'
+  service 'postgresql' do
+    supports :status => true, :restart => true, :reload => true
+    action [:enable, :start]
+  end
+else
+  service 'postgresql' do
+    action [:disable, :stop]
+  end
 end
 
 service 'ambari-server' do
-  status_command "/etc/init.d/ambari-server status | grep 'Ambari Server running'"
-  action [:enable, :start]
+  status_command 'service ambari-server status'
+  supports :status => true, :restart => true, :reload => false
+  action [:start, :enable]
 end
