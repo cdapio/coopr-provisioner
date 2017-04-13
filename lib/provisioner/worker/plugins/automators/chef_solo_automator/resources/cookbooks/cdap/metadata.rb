a41cfa23cd6c2949c97e798ef15c45ccbfe20ba6
Matt Wuenschel <matt@cask.co>
2017-04-13 16:13:58 -0700

    Import cdap version 3.0.2

--- lib/provisioner/worker/plugins/automators/chef_solo_automator/resources/cookbooks/cdap/metadata.rb
+++ lib/provisioner/worker/plugins/automators/chef_solo_automator/resources/cookbooks/cdap/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@cask.co'
 license          'Apache 2.0'
 description      'Installs/Configures Cask Data Application Platform (CDAP)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '3.0.1'
+version          '3.0.2'
 
 %w(ambari ark apt java nodejs ntp yum).each do |cb|
   depends cb
@@ -19,3 +19,4 @@ end
 
 source_url 'https://github.com/caskdata/cdap_cookbook' if respond_to?(:source_url)
 issues_url 'https://issues.cask.co/browse/COOK/component/10603' if respond_to?(:issues_url)
+chef_version '>= 11' if respond_to?(:chef_version)
