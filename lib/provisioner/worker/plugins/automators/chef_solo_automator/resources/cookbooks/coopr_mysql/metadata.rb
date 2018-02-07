c00e0c00023b61121b6d5342fa5bd07df5d10052
Derek Wood <derek@cask.co>
2018-02-06 21:21:46 -0800

    default mysql to 5.5

--- lib/provisioner/worker/plugins/automators/chef_solo_automator/resources/cookbooks/coopr_mysql/metadata.rb
+++ lib/provisioner/worker/plugins/automators/chef_solo_automator/resources/cookbooks/coopr_mysql/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@cask.co'
 license          'All rights reserved'
 description      'Manage MySQL database instances'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.0'
+version '0.3.0'
 
 depends 'mysql', '~> 8.0'
 depends 'yum', '>= 3.0'
