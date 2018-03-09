dd5f464504a6117c566e3046ee1ecc9cefcfac74
Derek Wood <derek@cask.co>
2018-03-09 15:47:08 -0800

    major version bump due to api/dependency update

--- lib/provisioner/worker/plugins/automators/chef_solo_automator/resources/cookbooks/coopr_dns/metadata.rb
+++ lib/provisioner/worker/plugins/automators/chef_solo_automator/resources/cookbooks/coopr_dns/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@cask.co'
 license          'All rights reserved'
 description      'Installs/Configures DNS'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.0'
+version          '1.0.0'
 
 depends 'dnsimple', '>= 2.0'
 depends 'dynect'
