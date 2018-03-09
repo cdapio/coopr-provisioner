2c3928f63bb541205a456460a7f6292098975f80
Derek Wood <derek@cask.co>
2018-03-09 02:15:47 -0800

    update coopr_dns dnsimple to api v2

--- lib/provisioner/worker/plugins/automators/chef_solo_automator/resources/cookbooks/coopr_dns/attributes/dnsimple.rb
+++ lib/provisioner/worker/plugins/automators/chef_solo_automator/resources/cookbooks/coopr_dns/attributes/dnsimple.rb
@@ -1,7 +1,3 @@
-# Specify a fog version
-default['dnsimple']['fog_version'] = '1.21.0'
 # Our configuration
 default['coopr_dns']['dnsimple']['databag_name'] = 'creds'
 default['coopr_dns']['dnsimple']['databag_item'] = 'dnsimple'
-# Ensure we run build-essential at compile-time
-override['build-essential']['compile_time'] = true
